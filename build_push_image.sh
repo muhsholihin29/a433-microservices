daf9f0246657eed8068e921befa190ef4f02b762
sholi <sholihin.ok1998@gmail.com>
2023-03-11 15:11:24 +0700

    revisi

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,8 +1,10 @@
 # build docker image from Dockerfile
 docker build -t item-app:v1 .
-# Running or build image with name mongo-db, using mode detach, use option network item-app, and volume app-data with directoy /data/db
-docker run -d --name mongo-db --network item-app --network-alias item-db -v app-db:/data/db mongo:3
-# Run container with mode detach and use host port and container port 8080, with the name item-app and using image item-app:v1
-docker run -dp 8080:8080  -w /app --network item-app --name item-app item-app:v1
 # Show list of docker images 
 docker images
+# create a new image tag from item-app:v1 to sholihin29/item-app:v1
+docker tag item-app:v1 sholihin29/item-app:v1
+# Login to docker hub with username and password
+docker login --username=sholihin29 --password=$DOCKER_PASS
+# Push image to docker hub
+docker push sholihin29/item-app:v1
